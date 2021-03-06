a8972e9f8f7d331cb8f1dbf3f6a87a7056c32995
Andrew Low <contact@ackl.me>
2021-03-06 16:01:28 +0800

    use SWR for ticker page

--- components/stock/stonkContainer.js
+++ components/stock/stonkContainer.js
@@ -8,104 +8,58 @@ import KeyStats from './keystats'
 import ChartComponent from '../chart/chartwrapper'
 import Grid from '@material-ui/core/Grid';
 
-export default class StonkContainer extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            fundemental: {},
-            OHLC: []
-        }
-    }
+const fetcher = url => fetch(url).then(r => r.json())
 
-    componentDidMount () {
-        if (!this.props.symbol) return
+export default function StonkContainer(props) {
+    const { data: fundemental } = useSWR(`${process.env.NEXT_PUBLIC_YAHOOFINANCEAPI}/stock/${props.symbol}`, fetcher)
 
-        fetch(`${process.env.NEXT_PUBLIC_YAHOOFINANCEAPI}/stock/${this.props.symbol}`)
-            .then((response) => response.json())
-            .then(data => {
-                this.setState(prevState => ({
-                    fundemental: data,
-                    OHLC: prevState.OHLC
-                }))
-            });
-
-        fetch(`${process.env.NEXT_PUBLIC_YAHOOFINANCEAPI}/stock/${this.props.symbol}/history`)
-            .then(x => x.json())
-            .then(data => {
-                data = data.map(x => {
-                    x.date = new Date(x.date)
-                    return x
-                })
-                console.log("CHART DATA", data)
-
-                this.setState(prevState => ({
-                    fundemental: prevState.fundemental,
-                    OHLC: data
-                }))
-            })
-
-        //this.timerID = setInterval(() => this.updatePrice(), 3000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
-    updatePrice() {
-        fetch(`${process.env.NEXT_PUBLIC_YAHOOFINANCEAPI}/stock/${this.props.symbol}/quote`)
-        .then((response) => response.json())
+    const { data: OHLC } = useSWR(`${process.env.NEXT_PUBLIC_YAHOOFINANCEAPI}/stock/${props.symbol}/history`, url => fetch(url)
+        .then(r => r.json())
         .then(data => {
-            this.setState(prevState => ({
-                fundemental: {
-                    ...prevState.fundemental,
-                    price: data
-                },
-                OHLC: data
-            }))
-        });
-    }
+            return data.map(x => {
+                x.date = new Date(x.date)
+                return x
+            })
+        })
+    )
 
-    render () {
-        if (this.state.fundemental) {
-            if (this.state.fundemental.message) {
-                return <p>{ this.state.fundemental.message }</p>
-            }
+    const { data: quote } = useSWR(`${process.env.NEXT_PUBLIC_YAHOOFINANCEAPI}/stock/${props.symbol}/quote`, fetcher, { refreshInterval: 3000 })
 
-            if (!this.state.fundemental.price) {
-                return <Spinner />
-            }
-
-            return (
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <h1>{ this.state.fundemental.price.symbol } [{ this.state.fundemental.price.exchangeName }]</h1>
-                        <a className="header-link" href={`https://finance.yahoo.com/quote/AAPL/${this.state.fundemental.price.symbol}`} target="_blank">Yahoo Finance</a>
-                        <a className="header-link" href={`https://www.marketwatch.com/investing/stock/${this.state.fundemental.price.symbol}`} target="_blank">MarketWatch</a>
-                    </Grid>
-
-                    <Grid item xs={12} md={3}>
-                        <Price price={this.state.fundemental.price} />
-                    </Grid>
-
-                    <Grid item xs={12} md={6}>
-                        <Profile price={this.state.fundemental.price} assetProfile={this.state.fundemental.assetProfile} esg={this.state.fundemental.esgScores} />
-                    </Grid>
-                    {this.state.fundemental.earnings &&
-                    <Grid item xs={12} md={3}>
-                        <EarningsChart earnings={this.state.fundemental.earnings} />
-                    </Grid>
-                    }
-                    <Grid item xs={12} md={9}>
-                        <ChartComponent data={this.state.OHLC} volume={true} />
-                    </Grid>
-
-                    <Grid item xs={12} md={3}>
-                        <KeyStats stats={this.state.fundemental.defaultKeyStatistics} summaryDetail={this.state.fundemental.summaryDetail} />
-                    </Grid>
-
-                </Grid>
-            )
-        }
+    if (!fundemental || !OHLC || !quote) {
         return <Spinner />
     }
+
+    if (fundemental.message) {
+        return <p>{ fundemental.message }</p>
+    }
+
+    return (
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <h1>{ fundemental.price.symbol } [{ fundemental.price.exchangeName }]</h1>
+                <a className="header-link" href={`https://finance.yahoo.com/quote/AAPL/${fundemental.price.symbol}`} target="_blank">Yahoo Finance</a>
+                <a className="header-link" href={`https://www.marketwatch.com/investing/stock/${fundemental.price.symbol}`} target="_blank">MarketWatch</a>
+            </Grid>
+
+            <Grid item xs={12} md={3}>
+                <Price price={quote} />
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+                <Profile price={fundemental.price} assetProfile={fundemental.assetProfile} esg={fundemental.esgScores} />
+            </Grid>
+            {fundemental.earnings &&
+            <Grid item xs={12} md={3}>
+                <EarningsChart earnings={fundemental.earnings} />
+            </Grid>
+            }
+            <Grid item xs={12} md={9}>
+                <ChartComponent data={OHLC} volume={true} />
+            </Grid>
+
+            <Grid item xs={12} md={3}>
+                <KeyStats stats={fundemental.defaultKeyStatistics} summaryDetail={fundemental.summaryDetail} />
+            </Grid>
+        </Grid>
+    )
 }
