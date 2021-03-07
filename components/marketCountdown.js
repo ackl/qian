b266a6120c1fcce1537e2f033eeff2641908405d
Andrew Low <contact@ackl.me>
2021-03-07 16:09:24 +0800

    handle market holidays

--- components/marketCountdown.js
+++ components/marketCountdown.js
@@ -12,44 +12,29 @@ const Countdown = dynamic(() => import('react-countdown'), {
     ssr: false
 })
 
-const US_MARKET_COUNTDOWN_DATA = getCountdownData('America/New_York', ['T09:30'], ['T16:00'])
-const UK_MARKET_COUNTDOWN_DATA = getCountdownData('Europe/London', ['T08:00', 'T12:02'], ['T12:00', 'T16:30'])
-const HK_MARKET_COUNTDOWN_DATA = getCountdownData('Asia/Hong_Kong', ['T09:30', 'T13:00'], ['T12:00', 'T16:00'])
+const MarketCountdownData = {
+    NYSE: getCountdownData('America/New_York', ['T09:30'], ['T16:00']),
+    LSE: getCountdownData('Europe/London', ['T08:00', 'T12:02'], ['T12:00', 'T16:30']),
+    HKEX: getCountdownData('Asia/Hong_Kong', ['T09:30', 'T13:00'], ['T12:00', 'T16:00'])
+}
 
 export default class MarketCountdown extends React.Component {
     render () {
-        return (
-            <>
-            <Grid item xs={12} md={2}>
-                <Card>
-                    <CardContent className={UK_MARKET_COUNTDOWN_DATA.marketEvent == 'open' ? styles.close : styles.open}>
-                        <h1>LSE</h1>
-                        <h1> <Countdown daysInHours date={new Date(UK_MARKET_COUNTDOWN_DATA.date.format())} /> </h1>
-                        <h2>until {UK_MARKET_COUNTDOWN_DATA.marketEvent}</h2>
-                    </CardContent>
-                </Card>
-            </Grid>
 
-            <Grid item xs={12} md={2}>
-                <Card>
-                    <CardContent className={US_MARKET_COUNTDOWN_DATA.marketEvent == 'open' ? styles.close : styles.open}>
-                        <h1>NYSE</h1>
-                        <h1> <Countdown daysInHours date={new Date(US_MARKET_COUNTDOWN_DATA.date.format())} /> </h1>
-                        <h2>until {US_MARKET_COUNTDOWN_DATA.marketEvent}</h2>
-                    </CardContent>
-                </Card>
-            </Grid>
+        const MarketCountdownCards = Object.keys(MarketCountdownData).map(k => {
+            return (
+                <Grid item xs={12} md={2}>
+                    <Card>
+                        <CardContent className={MarketCountdownData[k].countdownToEvent == 'open' ? styles.close : styles.open}>
+                            <h1>{k}</h1>
+                            <h1> <Countdown daysInHours date={new Date(MarketCountdownData[k].date.format())} /> </h1>
+                            <h2>until {MarketCountdownData[k].countdownToEvent}</h2>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            )
+        })
 
-            <Grid item xs={12} md={2}>
-                <Card>
-                    <CardContent className={HK_MARKET_COUNTDOWN_DATA.marketEvent == 'open' ? styles.close : styles.open}>
-                        <h1>HKEX</h1>
-                        <h1> <Countdown daysInHours date={new Date(HK_MARKET_COUNTDOWN_DATA.date.format())} /> </h1>
-                        <h2>until {HK_MARKET_COUNTDOWN_DATA.marketEvent}</h2>
-                    </CardContent>
-                </Card>
-            </Grid>
-            </>
-        )
+        return MarketCountdownCards
     }
 }
--- components/utils.js
+++ components/utils.js
@@ -1,51 +1,132 @@
 import moment from 'moment-timezone'
 
-//TODO
 const HOLIDAYS = {
+    'Asia/Hong_Kong': {
+        '2021-04-02': false,
+        '2021-04-05': false,
+        '2021-04-06': false,
+        '2021-05-19': false,
+        '2021-06-14': false,
+        '2021-07-01': false,
+        '2021-09-22': false,
+        '2021-10-14': false,
+        '2021-12-24': ['T09:30', 'T12:00'],
+        '2021-12-27': false,
+        '2021-12-31': ['T09:30', 'T12:00'],
+    },
+
+    'Europe/London': {
+        '2021-04-02': false,
+        '2021-04-05': false,
+        '2021-05-03': false,
+        '2021-05-31': false,
+        '2021-08-30': false,
+        '2021-12-24': ['T08:00', 'T12:30'],
+        '2021-12-27': false,
+        '2021-12-28': false,
+        '2021-12-31': ['T08:00', 'T12:30'],
+    },
+
+    'America/New_York': {
+        '2021-04-02': false,
+        '2021-05-31': false,
+        '2021-07-05': false,
+        '2021-09-06': false,
+        '2021-10-25': false,
+        '2021-10-26': ['T09:30', 'T13:00'],
+        '2021-12-24': false,
+    }
+}
+
+function checkIfHoliday(date, market_tz) {
+    const day = date.isoWeekday()
+
+    // check if weekend
+    if (day == 6 || day == 7) {
+        return true;
+    }
+
+    if (date.format('YYYY-MM-DD') in HOLIDAYS && !HOLIDAYS[date.format('YYYY-MM-DD')]) {
+        return true;
+    }
+}
+
+function checkIfPartial(date, market_tz) {
+    if (date.format('YYYY-MM-DD') in HOLIDAYS && HOLIDAYS[date.format('YYYY-MM-DD')]) {
+        return HOLIDAYS[date.format('YYYY-MM-DD')]
+    }
 }
 
 export function getCountdownData(market_tz, open, close) {
     const now = moment();
     const market_now = now.clone().tz(market_tz)
-    const marketDate = market_now.format('YYYY-MM-DD')
 
-    let marketOpen = moment.tz(`${marketDate}${open[0]}`, market_tz)
-    let marketClose = moment.tz(`${marketDate}${close[close.length - 1]}`, market_tz)
+    // If we're on a market holiday, skip days until we're not
+    while (checkIfHoliday(market_now, market_tz)) {
+        market_now.add(1, 'd')
+    }
 
-    let marketClosePM = moment.tz(`${marketDate}${close[0]}`, market_tz)
-    let marketOpenPM = moment.tz(`${marketDate}${open[open.length - 1]}`, market_tz)
+    const DATE_STRING = market_now.format('YYYY-MM-DD')
 
-    let countdownDate = marketClose;
-    let marketEvent = 'close';
+    let marketOpen = moment.tz(`${DATE_STRING}${open[0]}`, market_tz)
+    let marketClose = moment.tz(`${DATE_STRING}${close[close.length - 1]}`, market_tz)
 
+    let marketClosePM = moment.tz(`${DATE_STRING}${close[0]}`, market_tz)
+    let marketOpenPM = moment.tz(`${DATE_STRING}${open[open.length - 1]}`, market_tz)
+
+    let countdownToDate = marketClose;
+    let countdownToEvent = 'close';
+
+    // See if we're on a partial trading hours day
+    const partial = checkIfPartial(market_now, market_tz)
+
+    if (partial) {
+        marketOpen = marketOpenPM = partial[0]
+        marketClose = marketClosePM = partial[1]
+    }
+
+    // Are we currently within opening hours?
     if (now.isBetween(marketOpen, marketClose)) {
+
+        // If market has midday break
         if (open.length > 1) {
-            let isLunch = now.isBetween(marketOpenPM, marketClosePM)
-            let isPM = now.isSameOrAfter(marketOpenPM)
+            const isLunch = now.isBetween(marketOpenPM, marketClosePM)
+            const isPM = now.isSameOrAfter(marketOpenPM)
 
             if (isLunch) {
-                countdownDate = marketOpenPM
-                marketEvent = 'open'
+                countdownToDate = marketOpenPM
+                countdownToEvent = 'open'
             } else if (!isPM) {
-                countdownDate = marketClosePM
-                marketEvent = 'close'
+                countdownToDate = marketClosePM
+                countdownToEvent = 'close'
             }
         }
     } else {
         if (now.isAfter(marketClose)) {
             const market_tmrw = market_now.add(1, 'd')
-            const marketDate = market_tmrw.format('YYYY-MM-DD')
 
-            marketOpen = moment.tz(`${marketDate}${open[0]}`, market_tz)
+            while (checkIfHoliday(market_tmrw, market_tz)) {
+                market_tmrw.add(1, 'd')
+            }
+
+            const marketNextDate = market_tmrw.format('YYYY-MM-DD')
+
+            const partialNext = checkIfPartial(market_tmrw, market_tz)
+
+            if (partialNext) {
+                marketOpen = partial[0]
+            }
+
+            marketOpen = moment.tz(`${marketNextDate}${open[0]}`, market_tz)
         }
 
-        countdownDate = marketOpen;
-        marketEvent = 'open';
+        countdownToDate = marketOpen;
+        countdownToEvent = 'open';
     }
 
     return {
-        date: countdownDate,
-        marketEvent
+        date: countdownToDate,
+        countdownToEvent
     }
 }
 
