1e35040719915d5a36796629024e5e0ae607151f
Andrew Low <contact@ackl.me>
2021-03-07 19:16:06 +0800

    nextjs wants link tag to be self closing

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,8 @@ class CustomDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="preconnect" href="https://fonts.gstatic.com">
-                    <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=swap" rel="stylesheet">
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=swap" rel="stylesheet" />
                 </Head>
                 <body>
                     <Main />
