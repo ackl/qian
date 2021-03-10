19b0daa5d3a418586bba70eade7ce07488fb3bd1
Andrew Low <contact@ackl.me>
2021-03-10 15:33:47 +0800

    enable CORS (again)

--- yahoo-finance-api-proxy/main.py
+++ yahoo-finance-api-proxy/main.py
@@ -1,15 +1,13 @@
 from yahooquery import Ticker
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def default():
     return jsonify({ "message": "This is not the droid you're looking for" })
-
-@app.route("/hi/")
-def hi():
-    return "Hi"
 
 @app.route("/stock/")
 def stock():
