90027928be81b2d6fbf51a903e6c3bbddeddb27b
Andrew Low <contact@ackl.me>
2021-03-10 15:30:07 +0800

    use AWS lambda for yahoofinance api proxy, vercel has routing issues for serverless flask functions

--- yahoo-finance-api-proxy/main.py
+++ yahoo-finance-api-proxy/main.py
@@ -1,9 +1,7 @@
 from yahooquery import Ticker
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route("/")
 def default():
--- yahoo-finance-api-proxy/wsgi.py
+++
@@ -1,4 +0,0 @@
-from chickentenders import app
-
-if __name__ == "__main__":
-    app.run()
